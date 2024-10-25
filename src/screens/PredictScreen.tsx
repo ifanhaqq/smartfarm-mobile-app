793241084a89e4923e1365da862d87035ea1afa0
eastID-10 <eastidmyt@gmail.com>
2024-10-25 12:54:05 +0700

    new predict screen

---
+++ src/screens/PredictScreen.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button, TouchableOpacity, Image } from 'react-native';
+import { Icon } from 'react-native-elements';
+import { LinearGradient } from 'expo-linear-gradient'; 
+
+const PredictScreen: React.FC = () => {
+    return ( 
+       <View>
+        
+       </View>
+
+    )
+}
+
+const styles = StyleSheet.create({
+   
+});
+
+export default PredictScreen;
