94afbf97bf894c2e5db2cc6a095c886bfa395a49
eastID-10 <eastidmyt@gmail.com>
2024-10-29 12:44:10 +0700

    progress

--- src/screens/MonthlyReportScreen.tsx
+++ src/screens/MonthlyReportScreen.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
+import { Icon } from 'react-native-elements';
+import { color } from 'react-native-elements/dist/helpers';
 import { ReportService } from 'src/services/ReportService';
 
 const MonthlyReportScreen: React.FC = () => {
@@ -21,7 +23,7 @@ const MonthlyReportScreen: React.FC = () => {
         { label: "Desember", value: 12 }
     ]);
     const [dropdownOpen, setDropdownOpen] = useState<boolean>(false);
-    const [reportData, setReportData] = useState<any[]>([{ column1: 'Kadar air tanah', column2: '% Air tanah tersedia' }])
+    const [reportData, setReportData] = useState<any[]>([])
     const [ats, setAts] = useState<number>(0);
 
     useEffect(() => {
@@ -55,6 +57,7 @@ const MonthlyReportScreen: React.FC = () => {
 
     return (
         <View style={styles.container}>
+
             <Text style={styles.title}>Laporan Bulanan Neraca Air</Text>
             <DropDownPicker
                 style={styles.dropdown}
@@ -69,20 +72,44 @@ const MonthlyReportScreen: React.FC = () => {
                 setItems={setDropdownItem}
                 setOpen={setDropdownOpen}
             />
-            <View style={styles.box}> 
+            <View style={styles.box}>
                 <View style={styles.table}>
+
+                    <Text style={{ color: '#616161', margin: 5, fontWeight: 'semibold', fontSize: 17 }} >Hasil laporan</Text>
+                    <View style={styles.col}>
+                        <View style={styles.col2}>
+                            <Image source={require("../assets/icon-soil.png")} style={styles.icon}></Image>
+                            <View style={{ flexDirection: 'column', marginStart: 5, }}>
+                                <Text style={{color:'#616161'}} > kadar air tanah</Text>
+                                <Text style={styles.value}>0.75</Text>
+                            </View>
+                        </View>
+                        <View style={styles.col2}>
+                            <Image source={require("../assets/icon-water.png")} style={{ width: 23, height: 30, }}></Image>
+                            <View style={{ flexDirection: 'column',  marginStart: 5,  }}>
+                                <Text style={{color:'#616161'}}> % air tanah tersedia</Text>
+                                <Text style={styles.value}>0.75</Text>
+                            </View>
+                        </View>
+                    </View>
                     {reportData.map((item: any, index: any) => (
                         <View key={index} style={styles.row}>
+
                             <Text style={styles.cell}> {item.column1}</Text>
+
                             <Text style={styles.cell}>{item.column2}</Text>
                         </View>
                     ))}
+
+                    {(ats <= 40 && ats >= 10) ? <Text style={styles.description}>Tidak disarankan untuk menanam padi</Text>
+                        : (ats <= 60 && ats >= 41) ? <Text style={styles.description}>Cukup tidak disarankan untuk menanam padi</Text>
+                            : (ats <= 90 && ats >= 61) ? <Text style={styles.description}>Sangat disarankan untuk menanam padi</Text>
+                                : (ats >= 91) ? <Text style={styles.description}>Cukup disarankan untuk menanam padi</Text>
+                                    : <Text style={styles.description}>Sangat tidak disarankan untuk menanam padi</Text>}
                 </View>
-                {(ats <= 40 && ats >= 10) ? <Text style={styles.description}>Tidak disarankan untuk menanam padi</Text>
-                    : (ats <= 60 && ats >= 41) ? <Text style={styles.description}>Cukup tidak disarankan untuk menanam padi</Text>
-                        : (ats <= 90 && ats >= 61) ? <Text style={styles.description}>Sangat disarankan untuk menanam padi</Text>
-                            : (ats >= 91) ? <Text style={styles.description}>Cukup disarankan untuk menanam padi</Text>
-                                : <Text style={styles.description}>Sangat tidak disarankan untuk menanam padi</Text>}
+            </View>
+            <View style={styles.row}>
+                <Image source={require('../assets/banner-1.png')} style={{width: 350, height: 100,}}></Image>
 
             </View>
         </View>
@@ -91,10 +118,10 @@ const MonthlyReportScreen: React.FC = () => {
 
 const styles = StyleSheet.create({
     container: {
+        margin: 10,
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        margin: 20,
     },
     dropdown: {
         borderWidth: 0,
@@ -103,40 +130,76 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.2,
         shadowRadius: 3,
         elevation: 10,
-
+        color: '#616161',
     },
     title: {
-        marginBottom: 10,
+        marginBottom: 50,
         borderColor: 'white',
+        color: '#545454',
+        fontWeight: 'semibold',
+        fontSize: 20,
     },
     table: {
-        margin: 20,
+        paddingTop: 10,
+        paddingStart: 10, 
+        backgroundColor: 'white',
+        borderRadius: 10,
+        color: '#616161',
+        marginStart: 20,
+        marginEnd: 20,
+
     },
     row: {
         flexDirection: 'row',
         marginStart: 25,
         marginEnd: 25,
         marginBottom: 10,
+        color: '#616161',
     },
     cell: {
         padding: 10,
-        borderWidth: 1,
+
         borderColor: '#ccc',
         textAlign: 'center',
         width: 'auto',
-        margin: 10,
+        rowGap: 5,
+        color: '#616161',
     },
     description: {
-        marginTop: 20
+        margin: 10,
+        padding: 10, 
+        borderWidth: 0.2,
+        borderRadius: 5,
+        borderColor: '#D9D9D9',
+        color: '#616161',
     },
-    box:{
-        backgroundColor: 'white',
-        borderRadius: 10, 
+    box: {
         justifyContent: 'center',
-        alignItems: 'center', 
-        padding:20,
-        marginTop:20,
-        
+        alignItems: 'center',
+        marginTop: 20,
+        margin: 20,
+
+    },
+    icon: {
+        width: 30,
+        height: 30,
+    },
+    col: {
+        flexDirection: 'row',
+        padding: 4,
+
+    },
+    col2: {
+        borderRadius: 10,
+        borderColor: '#D9D9D9',
+        borderWidth: 0.2,
+        flexDirection: 'row',
+        padding: 10,
+        marginEnd: 5,
+
+    },
+    value: {
+        color: '#407BFF',
     },
 });
 
