894e702465babfd32e8b34503ec4cd5ffa6c1216
eastID-10 <eastidmyt@gmail.com>
2024-10-29 13:33:05 +0700

    field

--- src/screens/FieldScreen.tsx
+++ src/screens/FieldScreen.tsx
@@ -1,43 +1,46 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button, TouchableOpacity, Image } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity, Image } from 'react-native';
 import { Icon } from 'react-native-elements';
 import { LinearGradient } from 'expo-linear-gradient';
 
 const FieldScreen: React.FC = () => {
     return (
-        <View style={styles.container}>
-            <View style={styles.box}>
-                <Text style={styles.h1}>Semua lahan</Text>
-                <View style={styles.col}>
 
-                    <View style={styles.table}>
-                        <Image style={styles.img}></Image>
-                    </View>
-                    <View style={styles.row}>
-                        <Text style={styles.cell}>Nama lahan </Text>
-                        <Text style={styles.cellTitle}>Lahan padi</Text>
-                    </View>
-                    <View style={styles.row}>
-                        <Text style={styles.cell}>Masa Tanam </Text>
-                        <Text style={styles.cellTitle}>175 Hari</Text>
-                    </View>
-                    <View style={styles.row}>
-                        <Text style={styles.cell}>Status Panen </Text>
-                        <Text style={styles.cellTitle}>Sudah</Text>
-                    </View>
-                    <LinearGradient colors={['#24C6DC   , #514A9D']} style={styles.cellBtn2}>
-                        <View style={styles.row}>
-                            <Text style={styles.cellBtn2} >Detail</Text>
-                            <Text style={styles.line}></Text>
+        <ScrollView style={styles.container}>
+            <View style={styles.wrapper}>
+                <View style={[styles.box]}>
+                    <Text style={styles.h1}>Semua lahan</Text>
+                    <View style={styles.col}>
+
+                        <View style={styles.table}>
+                            <Image style={styles.img}></Image>
                         </View>
-                    </LinearGradient>
+                        <View style={styles.row}>
+                            <Text style={styles.cell}>Nama lahan </Text>
+                            <Text style={styles.cellTitle}>Lahan padi</Text>
+                        </View>
+                        <View style={styles.row}>
+                            <Text style={styles.cell}>Masa Tanam </Text>
+                            <Text style={styles.cellTitle}>175 Hari</Text>
+                        </View>
+                        <View style={styles.row}>
+                            <Text style={styles.cell}>Status Panen </Text>
+                            <Text style={styles.cellTitle}>Sudah</Text>
+                        </View>
+                        <LinearGradient colors={['#24C6DC   , #514A9D']} style={styles.cellBtn2}>
+                            <View style={styles.row}>
+                                <Text style={styles.cellBtn2} >Detail</Text>
+                                <Text style={styles.line}></Text>
+                            </View>
+                        </LinearGradient>
+                    </View>
+                </View>
+
+                <View style={styles.box2}>
+                    <Image source={require('../assets/vektor-lahan.png')} style={styles.img2}></Image>
                 </View>
             </View>
-
-            <View style={styles.box2}>
-                <Image source={require('../assets/vektor-lahan.png')} style={styles.img2}></Image>
-            </View>
-        </View>
+        </ScrollView>
 
 
     )
@@ -45,24 +48,25 @@ const FieldScreen: React.FC = () => {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'flex-start',
-        alignItems: 'flex-start',
         backgroundColor: '#DDECF8',
-        opacity: 0.8,
+    },
 
-
+    wrapper: {
+        backgroundColor: '#5C93E0',
+        marginStart: 20,
+        // marginRight: 20,
+        marginTop: 20,
+        width: 340,
+        borderRadius: 15,
     },
 
     box: {
-        marginStart: 20,
-        marginEnd: 20,
-        marginTop: 20,
-        backgroundColor: 'white',
+        
+        backgroundColor: '#fff',
         padding: 5,
-        width: 340,
-        borderRadius: 15,
-        shadowColor: '#000000',    // Shadow color
+        // borderWidth: 0.4,
+        borderColor: 'grey',
+        borderRadius: 15, // Shadow color
 
     },
 
@@ -126,9 +130,9 @@ const styles = StyleSheet.create({
         textAlign: 'center',
     },
     box2: {
-        backgroundColor: '#5C93E0',
+        // backgroundColor: '#5C93E0',
         width: 350,
-        marginStart: 15, 
+        marginStart: 15,
         borderRadius: 10,
     },
     img2: {
